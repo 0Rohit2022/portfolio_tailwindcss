cf308e81e6ae3d2188e1843dbc6934f92bf1c0ae
Rohit kumar <rohitkumarrr2022@gmail.com>
2024-02-18 12:39:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import SocialLinks from "./components/SocialLinks";
+
+const App = () => {
+  return(
+    <>
+      <Navbar />
+      <Home />
+
+      <SocialLinks />
+    </>
+  )
+}
+
+export default App;
